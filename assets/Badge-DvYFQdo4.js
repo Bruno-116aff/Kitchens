e5310e942aae8f67f5a56772542de375e38a14bc
Bruno-116aff <Bruno-116aff@users.noreply.github.com>
2025-10-25 11:23:56 +0000

    deploy: 093d74be67e3985aa7f7b4f2766fe5f313cd7d76

---
+++ assets/Badge-DvYFQdo4.js
@@ -0,0 +1,1 @@
+import{j as d}from"./index-CMPOtVqq.js";const r=({children:a,variant:e="default",size:s="md",className:o=""})=>{const t=["badge",`badge--${e}`,`badge--${s}`,o].filter(Boolean).join(" ");return d.jsx("span",{className:t,children:a})};export{r as B};
