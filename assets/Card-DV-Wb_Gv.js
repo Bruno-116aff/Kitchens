5f421857eecf2223e8ade305f8b5238bf5e4a707
Bruno-116aff <Bruno-116aff@users.noreply.github.com>
2025-10-27 14:39:59 +0000

    deploy: d1a897413060c591e780959ff5a3560b63892947

--- assets/Card-DV-Wb_Gv.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DYQTZkuy.js";const C=({children:s,className:e="",hover:a=!1,...o})=>{const r=["card",a?"card--hover":"",e].filter(Boolean).join(" ");return t.jsx("div",{className:r,...o,children:s})};export{C};
